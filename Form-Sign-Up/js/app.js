216a8ec34fe1666f8b32c53b52c67fc0ee698c75
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-08-13 22:04:02 -0300

    Adicionando responsividade

--- Form-Sign-Up/js/app.js
+++ Form-Sign-Up/js/app.js
@@ -9,5 +9,5 @@ document.querySelector('.x-btn').addEventListener('click', () =>{
 	document.querySelector('.banner').style.display = 'flex';
   /*Para alterar duas ou mais propriedades no css, basta adicionar cssText e passar um texto css*/
   document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: hidden';
-  document.querySelector('.container').style.cssText = "background: linear-gradient(rgba(0,0,0, .8), rgba(0,0,0, .7) ), url('../images/bg1.jpeg') center no-repeat; background-size: cover";
+  document.querySelector('.container').style.cssText = "background: linear-gradient(rgba(0,0,0, .8), rgba(0,0,0, .7) ), url('images/bg1.jpg') center no-repeat; background-size: cover";
 });
